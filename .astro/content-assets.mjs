5e2e438374c6f5d2a33a7469f1658b13dab69086
gagadanalyst <gsinghmath@proton.me>
2025-10-06 20:59:23 -0400

    Mr. Data Engineer portfolio site based on Astro  first commit

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_20ube from "./s3bucket.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fs3-bucket-types%2Findex.md";
+export default new Map([["./s3bucket.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fs3-bucket-types%2Findex.md", __ASTRO_IMAGE_IMPORT_20ube]]);
+		
